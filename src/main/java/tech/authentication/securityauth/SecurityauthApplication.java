e5c7949ea840a9d03dd0df571de8c1e100684aac
TojiHayha <fernando.aq502@gmail.com>
2024-10-04 23:43:08 -0300

    Initial Commit SecurityAuth

---
+++ src/main/java/tech/authentication/securityauth/SecurityauthApplication.java
@@ -0,0 +1,13 @@
+package tech.authentication.securityauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityauthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tech/authentication/securityauth/SecurityauthApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.authentication.securityauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
